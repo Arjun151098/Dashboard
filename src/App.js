70c0eaf7bbde5e79bef243c84dff4aa13b47dd06
arjun-1510 <Arjung1510@gmail.com>
2024-10-16 20:18:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import EmpTable from './components/empTable/emp'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import EmpNavbar from './components/Navbar/Navbar';
+import { EmployeeTable } from './components/Table/EmployeeTable';
+
+function App () {
+
+
+  return(
+        <div>
+          <EmpNavbar />
+               <EmployeeTable />
+        </div>
   );
 }
 
---
+++ src/components/Table/EmployeeTable.js
@@ -0,0 +1,56 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+
+export const EmployeeTable = () => {
+
+
+    
+        const [users,setUsers] = useState([]);
+
+
+
+
+        useEffect( () =>    {
+           
+         axios.get("https://jsonplaceholder.typicode.com/users").then( (res) => setUsers(res.data)).catch((err) => console.log(err));
+
+         
+
+},[])
+
+
+        console.log(users)
+  return (
+
+
+
+
+
+  
+    <div className='container'>
+              <div className='table'>
+
+                <Table>
+                <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+                  {users.map(user => <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                  </tr>)}
+                  </tbody>
+                </Table>
+                       
+              </div>
+    </div>
+  )
+}
